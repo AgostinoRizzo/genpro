d69a00276adae50a460df7ce2a036854a882c4db
Agostino Rizzo <rzzgtn96p08d086j@studenti.unical.it>
2024-08-31 00:14:41 +0200

    Normalize objectives in front tracker.

--- src/backprop/pareto_front.py
+++ src/backprop/pareto_front.py
@@ -195,11 +195,25 @@ class DataLengthFrontTracker:
             return a[1] - b[1]
         sorted_front = sorted(self.front[frontidx], key=cmp_to_key(fcmp))
 
+        all_lengths = np.array([length for _, data, length in sorted_front])
+        length_min = all_lengths.min()
+        length_max = all_lengths.max()
+        length_span = length_max - length_min
+
         crowdist[id(sorted_front[0 ][0])] = np.infty
         crowdist[id(sorted_front[-1][0])] = np.infty
         for i, (stree, data, length) in enumerate(sorted_front[1:len(sorted_front)-1]):
             i += 1
-            crowdist[id(stree)] = sorted_front[i+1][1] - sorted_front[i-1][1] + sorted_front[i+1][2] - sorted_front[i-1][2]
+
+            data_next = sorted_front[i+1][1]  # already normalized (R2 score).
+            data_prev = sorted_front[i-1][1]
+
+            length_next = sorted_front[i+1][2]
+            length_prev = sorted_front[i-1][2]
+            length_next = (length_next - length_min) / length_span  # normalize length objective.
+            length_prev = (length_prev - length_min) / length_span
+
+            crowdist[id(stree)] = data_next - data_prev + length_next - length_prev
         
         return crowdist
 
