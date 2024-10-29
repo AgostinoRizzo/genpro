Fix backprop rules about symmetry.

src/symbols/unaop.py
@@ -167,7 +167,11 @@ class UnaryOperatorSyntaxTree(SyntaxTree):
         
         k_pulled = np.full(k_target.shape, np.nan)
         noroot_pulled = False
-        symm_pulled = symm_target  # symmetry (w.r.t. variables) backprop.
+        symm_pulled = None  # symmetry (w.r.t. variables) backprop.
+        if symm_target is not None and self.operator == 'square':
+            symm_pulled = (None, symm_target[1])
+        else:
+            symm_target
 
         if self.operator == 'square':
             if (k_target < 0.0).any():

tests/gp/test_corrector.py
@@ -94,7 +94,7 @@ backprop_node = ConstantSyntaxTree(2.0)
             backprop_node
         ),
         True,
-        True,
+        None,  # symm
         [np.nan] * 10 + ([np.nan] + [1.] * 9) * 9,
         {(0,): [np.nan] * 100, (1,): [np.nan] * 100},
         True,
@@ -114,7 +114,7 @@ backprop_node = ConstantSyntaxTree(2.0)
             )
         ),
         True,
-        False,
+        None,  # symm
         [np.nan] * 100,
         {(0,): [np.nan] * 100, (1,): [np.nan] * 100},
         True,
@@ -137,7 +137,7 @@ backprop_node = ConstantSyntaxTree(2.0)
             )
         ),
         True,
-        True,
+        None,  # symm
         [np.nan] * 100,
         {(0,): [np.nan] * 100, (1,): [np.nan] * 100},
         True,
@@ -163,7 +163,7 @@ backprop_node = ConstantSyntaxTree(2.0)
             )
         ),
         True,
-        True,
+        None,  # symm
         [np.nan] * 10 + ([np.nan] + [1.] * 9) * 9,
         {(0,): [np.nan] * 100, (1,): [np.nan] * 100},
         True,
@@ -190,7 +190,7 @@ def test_corrector(data, stree, noroot, symm, k_image, k_deriv, partial, none, r
 
     assert C_pulled.noroot == noroot
     assert (C_pulled.symm is None and symm is None) or C_pulled.symm[0] == symm
-    assert C_pulled.symm is None or C_pulled.symm[0] == is_symmetric(new_stree[mesh.X, ()], C_pulled.symm[1])
+    assert C_pulled.symm is None or C_pulled.symm[0] is None or C_pulled.symm[0] == is_symmetric(new_stree[mesh.X, ()], C_pulled.symm[1])
     assert set(C_pulled.origin_pconstrs.keys()) == set([()] + list(k_deriv.keys()))
 
     assert np.array_equal(C_pulled.origin_pconstrs[()], np.array(k_image), equal_nan=True)